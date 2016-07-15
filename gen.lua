605619e80fc9659f8d2f2ccf81ed3377cfe393b5
Jens John <dev@2ion.de>
2016-07-15 23:20:16 +0900

    gen:_mpv_objarg:fix greedy case selection(again)

--- gen.lua
+++ gen.lua
@@ -383,7 +383,7 @@ _mpv_objarg(){
   
   # Parameter arguments II:
   # Fragment completion
-  elif [[ ! $p =~ , && ( $p =~ : && ${p##*:} =~ = ) ]]; then
+  elif [[ ${p##*,} =~ : && ${p##*:} =~ = ]]; then
     # current filter
     s=${p##*,}
     s=${s%%:*}
